7433ef0164355a01ace711014ba6e0e4c74885d8
Philemon Ativor <pa1ativor@gamil.com>
2024-07-09 15:16:32 -0400

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 
-const PORT = 8080;
+const PORT =process.env.PORT|| 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
